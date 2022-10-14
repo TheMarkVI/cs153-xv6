1e9e533d833e768c32010e93f97a3ff38e045465
TheMarkVI <arnak9167@gmail.com>
2022-10-13 21:12:40 -0700

    Init

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include "types.h"
+#include "user.h"
+
+int
+main (int argc, char **argv) {
+    printf(1, "hello cs153!!!\n"); // fd=1 means standard output
+    exit();
+}
---
+++ test.c
@@ -0,0 +1,13 @@
+//
+// Created by ArtN9 on 10/6/2022.
+//
+
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(int argc, char *argv[]) {
+    //printf(1, "hello world\n");
+    hello(); // Call function from lab 0
+    exit();
+}
